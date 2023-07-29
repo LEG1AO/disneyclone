8c49350f91ffe21952cb9b4ff0f4f2693ddf0b43
leonardo vitor <legiongameplay@gmail.com>
2023-07-29 13:26:58 -0300

    ajuste no mobile

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 document.addEventListener('DOMContentLoaded',function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY
+
+        if(posicaoAtual < alturaHero){
+            ocultaElementosDoHeader();
+            } else {
+                exibeElementosdoHeader();
+            }
+    })
+
+    //Seção de atrações, programação das abas
+
     for (let i = 0; i <buttons.length; i++){
         buttons[i].addEventListener('click',function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
@@ -12,7 +29,28 @@ document.addEventListener('DOMContentLoaded',function(){
             
         })
     }
+
+    // Seção FAQ, accordion
+    for (let i =0; i < questions.length; i++) {
+        questions [i].addEventListener('click', abreoufecharesposta);
+    }
 })
+function abreoufecharesposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header')
+    header.classList.add ('header--is-hidden')
+}
+
+function exibeElementosdoHeader() {
+    const header = document.querySelector('header')
+    header.classList.remove ('header--is-hidden')
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
